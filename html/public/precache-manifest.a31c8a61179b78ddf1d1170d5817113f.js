1cd30ae82f6a463e902388f9a06270a48dbdfefe
5ym <16yuim@gmail.com>
2019-11-03 05:04:51 +0900

    add few function

---
+++ html/public/precache-manifest.a31c8a61179b78ddf1d1170d5817113f.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e11181cec591960f9c",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "48e11181cec591960f9c",
+    "url": "/js/app.js"
+  }
+]);
---
+++ html/public/precache-manifest.c46d740312d7843c3b491d612f195b81.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4d8eb976e235535cf0",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "3b4d8eb976e235535cf0",
+    "url": "/js/app.js"
+  }
+]);
